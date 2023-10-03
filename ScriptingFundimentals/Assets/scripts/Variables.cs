97258790200a532a7aa084225630efaad839e099
thenoobisme102 <thenoobisme102@gmail.com>
2023-10-03 14:23:06 -0700

    creating new variables

---
+++ ScriptingFundimentals/Assets/scripts/Variables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Public Global/Member Variables
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; // decimal number
+    public string PlayerName = "John Doe"; // declare text
+    public bool IsPlayerActive = true; // declare a true/false
+
+    // Private Global/Member Variables
+    private int _collectableAmount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
